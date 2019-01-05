f59a0ec08850bb86f2f2ee1cac23a92f2b413019
Tushar Vengurlekar <tushaarv@gmail.com>
2019-01-05 16:45:45 +0530

    Setup Mongoose and Body Parser

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 // Imports
 let express = require('express')
-let apiRoutes = require("./api-routes")
+let apiRoutes = require('./api-routes')
+let bodyParser = require('body-parser')
+let mongoose = require('mongoose')
 
 // Initialize the app
 let app = express();
+
+// Configure body parser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(bodyParser.json())
+
+// Connect to Mongoose and set connection variables
+mongoose.connect('mongodb://localhost/resthub');
+var db = mongoose.connection;
 
 // Setup server port
 var port = process.env.port || 8080;
